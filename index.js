75f237e17fbbd2a7476058261e183f02fe82d2b7
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-04 15:02:05 -0400

    Moving some things around

--- index.js
+++ index.js
@@ -1,126 +1,24 @@
 import cors from 'cors';
 import express from 'express';
-import uuidv4 from 'uuid/v4';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer} from 'apollo-server-express';
+
+import schema from 'graphql/schema.js'
+import resolvers from 'graphql/resolvers.js'
+import models from 'graphql/models.js'
 
 const app = express();
 
 app.use(cors());
 
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
 
-    messages: [Message!]!
-    message(id: ID!): Message!
-  }
 
-  type Mutation {
-    createMessage(text: String!): Message!
-    deleteMessage(id: ID!): Boolean!
-  }
-
-  type User {
-    id: ID!
-    username: String!,
-    messages: [Message!]
-  }
-
-  type Message {
-    id: ID!
-    text: String!
-    user: User!
-  }
-`;
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Jon Palacio',
-    messageIds: [1]
-  },
-  2: {
-    id: '2',
-    username: 'Dain',
-    messageIds: [2]
-  }
-}
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1'
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2'
-  }
-}
-
-const me = users[1]
-
-const resolvers = {
-  Query: {
-    users: () => {
-      return Object.values(users)
-    },
-    user: (parent, { id }) => {
-      return users[id]
-    },
-    me: (parent, args, { me }) => {
-      return me;
-    },
-    messages: () => {
-      return Object.values(messages);
-    },
-    message: (parent, { id }) => {
-      return messages[id]
-    }
-  },
-
-  Mutation: {
-    createMessage: (parent, { text }, { me }) => {
-      const id = uuidv4();
-      const message = {
-        id,
-        text,
-        userId: me.id
-      }
-      messages[id] = message;
-      users[me.id].messageIds.push(id)
-      return message;
-    },
-    deleteMessage: (parent, { id }) => {
-      const { [id]: message, ...otherMessages } = messages;
-      if (!message) {
-        return false;
-      }
-      messages = otherMessages;
-      return true;
-    }
-  },
-
-  Message: {
-    user: message => {
-      return users[message.userId];
-    }
-  },
-  User: {
-    messages: user => {
-      return Object.values(messages).filter(message => message.userId === user.id)
-    }
-  }
-}
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   context: {
-    me: users[1]
+    models,
+    me: models.users[1]
   }
 })
 
